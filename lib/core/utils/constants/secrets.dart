d04740b200a34b514f8193034faa31523d865af1
AzaKhaled <azakhaled813@gmail.com>
2026-08-12 11:34:40 +0300

    update payment screens

---
+++ lib/core/utils/constants/secrets.dart
@@ -0,0 +1,7 @@
+class Secrets {
+  static const String myfatoorahApiKey = 'SK_SAU_SE7w16AyzTrhROLHNKi4lsa1xgweR8V5xCmdZWqOpoL3AtQFnWYOJVXljtotTHtS';
+  static const String myfatoorahBaseUrl = 'https://api-sa.myfatoorah.com/';
+  static const String myfatoorahPortalHost = 'https://sa.myfatoorah.com/';
+  static const String myfatoorahCallbackUrl = 'http://localhost:3000/payment/myfatoorah/callback';
+  static const String myfatoorahErrorUrl = 'http://localhost:3000/payment/myfatoorah/error';
+}
